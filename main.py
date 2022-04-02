e92347ccf9d64abe028fd6316662d88f8bbd3b12
MrFlava <thatelitemaili33t@gmail.com>
2022-04-02 12:55:02 +0300

    Added __init__.py, database.py, models.py and schemas.py, updated main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # Create simple CRUD API with models, admin panel:
 # Make an Initial Commit on the GitHub
-# Create User model
 # Create auth
 # Create posts model
 # Create comments thread
---
+++ schemas.py
@@ -0,0 +1,19 @@
+from typing import List
+import pydantic as _pydantic
+
+
+class _UserBase(_pydantic.BaseModel):
+    email: str
+
+
+class UserCreate(_UserBase):
+    password: str
+
+
+class User(_UserBase):
+    id: int
+    is_active: bool
+
+    class Config:
+        orm_mode = True
+
